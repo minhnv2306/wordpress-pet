b4dc9d73e4f6da6117248d292873501f62cdb02a
minhnv2306 <minhnv2306@gmail.com>
2024-06-18 21:43:46 +0700

    feat: 100. Per-User Post Limit

--- section19/themes/fictional-university-theme/build/index.asset.php
+++ section19/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => 'c52fa03d667614bd06ed450c091c1a1c');
+<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '35e332d3928306a108786f6e5c502902');
--- section19/themes/fictional-university-theme/build/index.js
+++ section19/themes/fictional-university-theme/build/index.js
@@ -6157,6 +6157,10 @@ class MyNotes {
         thisNote.slideUp();
         console.log("Congrats");
         console.log(response);
+
+        if (response.userNoteCount < 5) {
+          jquery__WEBPACK_IMPORTED_MODULE_0___default()(".note-limit-message").removeClass("active");
+        }
       },
       error: response => {
         console.log("Sorry");
@@ -6218,6 +6222,10 @@ class MyNotes {
         console.log(response);
       },
       error: response => {
+        if (response.responseText == "You have reached your note limit.") {
+          jquery__WEBPACK_IMPORTED_MODULE_0___default()(".note-limit-message").addClass("active");
+        }
+
         console.log("Sorry");
         console.log(response);
       }
