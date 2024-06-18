5b476ef26b14948aa288e5b3001b06bb0e72a00a
minhnv2306 <minhnv2306@gmail.com>
2024-06-18 21:58:18 +0700

    feat: 125. Adding a Settings Page For Our Plugin

--- section23/our-first-unique-plugin/our-first-unique-plugin.php
+++ section23/our-first-unique-plugin/our-first-unique-plugin.php
@@ -8,12 +8,23 @@
   Author URI: https://www.udemy.com/user/bradschiff/
 */
 
-add_filter('the_content', 'addToEndOfPost');
-
-function addToEndOfPost($content) {
-  if (is_page() && is_main_query()) {
-    return $content . '<p>My name is Brad.</p>';
+class WordCountAndTimePlugin {
+  function __construct() {
+    add_action('admin_menu', array($this, 'adminPage'));
   }
 
-  return $content;
+  function adminPage() {
+    add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
+  }
+
+  function ourHTML() { ?>
+    <div class="wrap">
+      <h1>Word Count Settings</h1>
+    </div>
+  <?php }
 }
+
+$wordCountAndTimePlugin = new WordCountAndTimePlugin();
+
+
+
