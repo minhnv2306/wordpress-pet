e6a9206263bf97ee87a9117e3143e5ced2e3a36b
minhnv2306 <minhnv2306@gmail.com>
2024-06-18 21:51:03 +0700

    feat: 109. Completing the LikeBox

--- section20/themes/fictional-university-theme/build/index.js
+++ section20/themes/fictional-university-theme/build/index.js
@@ -6092,7 +6092,7 @@ class Like {
   ourClickDispatcher(e) {
     var currentLikeBox = jquery__WEBPACK_IMPORTED_MODULE_0___default()(e.target).closest(".like-box");
 
-    if (currentLikeBox.data("exists") == "yes") {
+    if (currentLikeBox.attr("data-exists") == "yes") {
       this.deleteLike(currentLikeBox);
     } else {
       this.createLike(currentLikeBox);
@@ -6110,6 +6110,11 @@ class Like {
         "professorId": currentLikeBox.data("professor")
       },
       success: response => {
+        currentLikeBox.attr("data-exists", "yes");
+        var likeCount = parseInt(currentLikeBox.find(".like-count").html(), 10);
+        likeCount++;
+        currentLikeBox.find(".like-count").html(likeCount);
+        currentLikeBox.attr("data-like", response);
         console.log(response);
       },
       error: response => {
@@ -6118,11 +6123,22 @@ class Like {
     });
   }
 
-  deleteLike() {
+  deleteLike(currentLikeBox) {
     jquery__WEBPACK_IMPORTED_MODULE_0___default.a.ajax({
+      beforeSend: xhr => {
+        xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
+      },
       url: universityData.root_url + "/wp-json/university/v1/manageLike",
+      data: {
+        "like": currentLikeBox.attr("data-like")
+      },
       type: "DELETE",
       success: response => {
+        currentLikeBox.attr("data-exists", "no");
+        var likeCount = parseInt(currentLikeBox.find(".like-count").html(), 10);
+        likeCount--;
+        currentLikeBox.find(".like-count").html(likeCount);
+        currentLikeBox.attr("data-like", "");
         console.log(response);
       },
       error: response => {
